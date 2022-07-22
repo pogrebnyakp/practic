8671c382e866d3e5962db6646cbc3b4071878f50
pogrebnyakp <pogrebnyakpavel@gmail.com>
2022-07-22 18:18:49 +0300

    cards

--- src/components/Cards/Cards.tsx
+++ src/components/Cards/Cards.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import './Cards.scss';
-import '../../pictures/Product__A__bag.png';
+import Product__A__bag from '../../pictures/Product__A__bag.png';
+import Product__B__blouse from '../../pictures/Product__B__blouse.png';
+import Product__C__jamper from '../../pictures/Product__C__jamper.png';
+import Product__D__sweater from '../../pictures/Product__D__sweater.png';
 
 export const Cards: React.FC = () => {
   return (
@@ -13,46 +16,58 @@ export const Cards: React.FC = () => {
 
           <ul className="cards__list list">
             <li className="cards__item card">
-              <img src="Product__A__bag.png" alt="bag" />
+
+              <img src={Product__A__bag} alt="bag" className="card__img" />
               <article className="card__description">
                 <p className="card__name list">Apollo Running Short</p>
                 <p className="card__price list">$50.00</p>
               </article>
+
             </li>
             <li className="cards__item card">
-              <img src="" alt="" />
+
+              <img src={Product__B__blouse} alt="blouse" className="card__img" />
               <article className="card__description">
                 <p className="card__name list">Apollo Running Short</p>
                 <p className="card__price list">$50.00</p>
               </article>
+
             </li>
             <li className="cards__item card">
-              <img src="" alt="" />
+
+              <img src={Product__C__jamper} alt="jamper" className="card__img" />
               <article className="card__description">
                 <p className="card__name list">Apollo Running Short</p>
                 <p className="card__price list">$50.00</p>
               </article>
+
             </li>
             <li className="cards__item card">
-              <img src="" alt="" />
+
+              <img src={Product__D__sweater} alt="sweater" className="card__img" />
               <article className="card__description">
                 <p className="card__name list">Apollo Running Short</p>
                 <p className="card__price list">$50.00</p>
               </article>
+
             </li>
             <li className="cards__item card">
-              <img src="" alt="" />
+
+              <img src={Product__A__bag} alt="bag" className="card__img" />
               <article className="card__description">
                 <p className="card__name list">Apollo Running Short</p>
                 <p className="card__price list">$50.00</p>
               </article>
+
             </li>
             <li className="cards__item card">
-              <img src="" alt="" />
+
+              <img src={Product__C__jamper} alt="jamper" className="card__img" />
               <article className="card__description">
                 <p className="card__name list">Apollo Running Short</p>
                 <p className="card__price list">$50.00</p>
               </article>
+
             </li>
           </ul>
 
